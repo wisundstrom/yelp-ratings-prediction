e6a3d1c137b99d28e643b9238715a81c49a831dc
wisundstrom <axe.sundstrom@gmail.com>
2019-11-14 18:00:56 -0500

    compared drop vs not in ohe

--- tn_helper.py
+++ tn_helper.py
@@ -18,15 +18,18 @@ def top_models(results):
     results_df.index=['rf_results', 'svm_mc_results', 'svm_ny_results', 'log_results']
     return results_df
 
-def feature_importance(model, X_train, categorical_features, numerical_features):
+def feature_importance(model, X_train, categorical_features, numerical_features, drop=True):
     """ This returns a dataframe with features and their importances. Input should be a
     fitted random forest model, the training data and the list of categorical features"""
     
     importances=model.feature_importances_
-    ohe=OneHotEncoder(drop='first').fit(X_train[categorical_features])
+    if drop:
+        ohe=OneHotEncoder(drop='first').fit(X_train[categorical_features])
+    else:
+        ohe=OneHotEncoder(drop=None).fit(X_train[categorical_features])
     categorical_names=ohe.get_feature_names()
     feature_names=numerical_features+list(categorical_names)
     importances_df=pd.concat([pd.DataFrame(importances),pd.DataFrame(feature_names)],axis=1)
     importances_df.columns=['Importance','Feature']
     importances_df=importances_df.sort_values(by='Importance',ascending=False)
-    return importances_df, ohe
+    return importances_df
